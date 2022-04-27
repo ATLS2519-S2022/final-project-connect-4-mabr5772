bdba457d0938ca3244426b74fa6ea7c52fadc259
mabr5772 <70348528+mabr5772@users.noreply.github.com>
2022-04-26 19:22:37 -0600

    Final

---
+++ src/GreedyPlayer.java
@@ -0,0 +1,147 @@
+/* *****************************************************************************
+ * Title:            (Greedy Player)
+ * Files:            (list of source files)
+ * Semester:         Spring 2022
+ * 
+ * Author:           (Kenzie Braun, mabr5772@colorado.edu)
+ * 
+ * Description:		 (This program creates a new AI  player)
+ * 
+ * Written:       	 (04/25/22)
+ * 
+ * Credits:          (Abby Hakewill, Apoorva Kanekal)
+ **************************************************************************** */
+public class GreedyPlayer implements Player {
+	private int cols;
+	private int id;
+	private int enemyId;
+
+	
+	private Move[] possibleMoves;
+
+	
+	public String name() {
+		return "Greedy";
+	}
+
+	public  void init(int id, int rows, int cols, int msecPerMove) {
+		enemyId = 3-id;
+		this.cols = cols;
+		this.id = id;
+
+	}
+
+	public void calcMove(Connect4Board board, int oppMoveCol, Arbitrator arb) throws TimeUpException{
+		if (board.isFull())
+			throw new Error("The board is full");
+
+		possibleMoves = new Move[cols];
+
+		for(int c = 0; c < cols; c++ ) {
+			if(board.isValidMove(c)) {
+				board.move(c ,id);
+				int moveValue = evaluateBoard(board, id, enemyId);
+				possibleMoves[c] = new Move(c, moveValue);
+				board.unmove(c, id);
+				
+				
+			}
+	}
+		
+		
+		Move bestMove = null;
+		for (int i = 0; i < possibleMoves.length; i++) {
+			if(bestMove == null)
+				bestMove = possibleMoves[i];
+
+			else if (possibleMoves[i] != null && bestMove.compareTo(possibleMoves[i]) < 0)
+				bestMove = possibleMoves[i];
+		
+		}
+		arb.setMove(bestMove.column);
+	}
+
+
+	private int evaluateBoard(Connect4Board board, int enemyId, int myId) {
+		int enemyScore = calcScore(board, enemyId);
+		int myScore = calcScore(board, myId);
+		
+		return myScore - enemyScore;
+	}
+
+
+	private int calcScore(Connect4Board board, int id) {
+		final int rows = board.numRows();
+		final int cols = board.numCols();
+		int score = 0;
+
+		// check vertically
+		for (int c = 0; c < cols; c++) {
+			for (int r = 0; r <= rows - 4; r++) {
+				if(board.get(r + 0, c) != id) continue;
+				if(board.get(r + 1, c) != id) continue;
+				if(board.get(r + 2, c) != id) continue;
+				if(board.get(r + 3, c) != id) continue;
+				score++;
+			}
+		}
+		
+		//check horizontally 
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c <= cols - 4; c++) {
+				if(board.get(r, c + 0) != id) continue;
+				if(board.get(r, c + 1) != id) continue;
+				if(board.get(r, c + 2) != id) continue;
+				if(board.get(r, c + 3) != id) continue;
+				score++;
+			}
+		}
+
+
+		// check diagonally 
+		for (int c = 0; c <= cols - 4; c++) {
+			for (int r = 0; r <= rows - 4; r++) {
+				if(board.get(r + 0, c + 0) != id) continue;
+				if(board.get(r + 1, c + 1) != id) continue;
+				if(board.get(r + 2, c + 2) != id) continue;
+				if(board.get(r + 3, c + 3) != id) continue;
+				score++;
+			}
+		}
+
+		// check diagonally 
+		
+		
+		for (int c = 0; c <= cols - 4; c++) {
+			for (int r = rows -1; r >= 3; r--) {
+				if (board.get(r - 0, c + 0) != id) continue;
+				if (board.get(r - 1, c + 1) != id) continue;
+				if (board.get(r - 2, c + 2) != id) continue;
+				if (board.get(r - 3, c + 3) != id) continue;
+				score++;
+			}
+		
+	}
+		
+		
+		return score;
+
+}
+
+	
+
+	private class Move implements Comparable<Move>{
+		private int value;
+		private int column;
+
+		
+		public Move(int column, int value) {
+			this.value = value;
+			this.column = column;
+		}
+
+		public int compareTo(Move other) {
+			return Integer.compare(this.value, other.value);
+		}
+	}
+}
---
+++ src/MinimaxPlayer.java
@@ -0,0 +1,214 @@
+/* *****************************************************************************
+ * Title:            (MinimaxPlayer)
+ * Files:            (list of source files)
+ * Semester:         Spring 2022
+ * 
+ * Author:           (Kenzie Braun, mabr5772@colorado.edu)
+ * 
+ * Description:		 (This program creates a new player that tries to find the optimal next move. )
+ * 
+ * Written:       	 (04/25/22)
+ * 
+ * Credits:          (Abby Hakewill, Apoorva Kanekal, https://www.baeldung.com/java-minimax-algorithm)
+ **************************************************************************** */
+
+import java.util.ArrayList;
+
+
+
+public class MinimaxPlayer implements Player {
+	private int enemyId;
+	private int id;
+	private int cols;
+	
+	
+	
+	
+	public String name() {
+		return "Minimax";
+	}
+
+	
+	public void init( int rows ,  int msecPerMove, int cols, int id) {
+		this.cols = cols;
+		this.enemyId = 3 - id;
+		this.id = id;
+		
+		
+		
+	  }
+	
+	public void calcMove(Connect4Board board, int oppMoveCol, Arbitrator arb) throws TimeUpException{
+		if(board.isFull())
+			throw new Error ("The board is full!");
+		GameTree root = new GameTree(-1, board);
+		
+		
+		for(int i= 0; i < cols; i++) {
+			if (!board.isColumnFull(i)) {
+				root.addChild(i, new Connect4Board(board));
+				board.move(i, id);
+				board.unmove(i, id);
+			}
+	}
+	
+		
+		int searchDepth = 1;
+		while (!arb.isTimeUp() && searchDepth <= board.numEmptyCells()) {
+			minimax(root, searchDepth, true, arb);
+			arb.setMove(root.chosenMove);
+			searchDepth++;
+		}
+		}
+	
+	private int minimax (GameTree node, int depth, boolean maxminizingPlayer, Arbitrator arb) {
+	if (depth == 0 || node.isTerminal() || arb.isTimeUp()) {
+		node.value = evaluateNode(node);
+		return node.value;
+}
+	
+	
+	if (node.isLeaf()) {
+		int moveId = maxminizingPlayer ? id : enemyId;
+
+		
+		for(int i = 0; i < cols; i++) {
+			if(!node.board.isColumnFull(i)) {
+				node.board.move(i, moveId);
+
+				node.addChild(i, new Connect4Board(node.board));
+				node.board.unmove(i, moveId);
+
+			}
+		}
+	}
+
+	
+	if(maxminizingPlayer) {
+		int value = Integer.MIN_VALUE;
+		for(GameTree child: node.children) {
+			int newVal = minimax(child, depth -1, false, arb);
+			if(newVal > value) {
+				value = newVal;
+				node.value = value;
+				node.chosenMove = child.move;
+			}
+			else if(newVal == value) {
+				int currMoveDistFromCenter = Math.abs(cols/2 - node.chosenMove);
+				int newMoveDistFromCenter = Math.abs(cols/2 - child.move);
+				if(newMoveDistFromCenter < currMoveDistFromCenter)
+					node.chosenMove = child.move;
+			}
+		}
+		return value;
+	}
+	
+	else {
+		int value = Integer.MAX_VALUE;
+		for(GameTree child: node.children) {
+			int newVal = minimax(child, depth -1, true, arb);
+			if(newVal < value) {
+				value = newVal;
+				node.value = value;
+				node.chosenMove = child.move;
+			}
+			else if (newVal == value) {
+				int currMoveDistFromCenter = Math.abs(cols/2 - node.chosenMove);
+				int newMoveDistFromCenter = Math.abs(cols/2 - child.move);
+				if(newMoveDistFromCenter < currMoveDistFromCenter)
+					node.chosenMove = child.move;
+			}
+
+		}
+		return value;
+	}
+
+		}
+
+
+private int evaluateNode(GameTree node) {
+	int myScore = calcScore(node.board, id);
+	int oppScore = calcScore(node.board, enemyId);
+	return myScore - oppScore;
+}
+
+
+private int calcScore(Connect4Board board, int id) {
+	final int rows = board.numRows();
+	final int cols = board.numCols();
+	int score = 0;
+
+	// check vertically
+	for (int c = 0; c < cols; c++) {
+		for (int r = 0; r <= rows - 4; r++) {
+			if(board.get(r + 0, c) != id) continue;
+			if(board.get(r + 1, c) != id) continue;
+			if(board.get(r + 2, c) != id) continue;
+			if(board.get(r + 3, c) != id) continue;
+			score++;
+		}
+	}
+	//check horizontally
+	for (int r = 0; r < rows; r++) {
+		for (int c = 0; c <= cols - 4; c++) {
+			if(board.get(r, c + 0) != id) continue;
+			if(board.get(r, c + 1) != id) continue;
+			if(board.get(r, c + 2) != id) continue;
+			if(board.get(r, c + 3) != id) continue;
+			score++;
+		}
+	}
+
+
+	// check diagonally
+	for (int c = 0; c <= cols - 4; c++) {
+		for (int r = 0; r <= rows - 4; r++) {
+			if(board.get(r + 0, c + 0) != id) continue;
+			if(board.get(r + 1, c + 1) != id) continue;
+			if(board.get(r + 2, c + 2) != id) continue;
+			if(board.get(r + 3, c + 3) != id) continue;
+			score++;
+		}
+	}
+
+	// check diagonally
+	for (int c = 0; c <= cols - 4; c++) {
+		for (int r = rows -1; r >= 3; r--) {
+			if (board.get(r - 0, c + 0) != id) continue;
+			if (board.get(r - 1, c + 1) != id) continue;
+			if (board.get(r - 2, c + 2) != id) continue;
+			if (board.get(r - 3, c + 3) != id) continue;
+			score++;
+		}
+	}
+	return score;
+
+}
+
+private class GameTree{
+	private Connect4Board board;
+	private int move;
+	private ArrayList<GameTree> children;
+	private int chosenMove;
+	private int value;
+
+	public GameTree(int move, Connect4Board board) {
+		this.move = move;
+		this.board = board;
+		children = new ArrayList<GameTree>();
+	}
+
+	
+	public void addChild(int move, Connect4Board board) {
+		children.add(new GameTree(move,board));
+	}
+
+	public boolean isLeaf() {
+		return children.size() == 0;
+	}
+
+	public boolean isTerminal() {
+		return board.isFull();
+	}
+}
+}
